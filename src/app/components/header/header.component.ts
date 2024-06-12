f76746c2f46877ca5afb5fac55c91a29f68abcc7
kshenukaishan <kshenukaishan@outlook.com>
2024-06-12 16:03:50 +0530

    header elemants buttons implemented

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MaterialModule } from '../../../module/MaterialModule';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MaterialModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
