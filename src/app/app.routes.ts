feb03ac9a3edfd22b61428df6d72b32bde640769
kshenukaishan <kshenukaishan@outlook.com>
2024-06-12 15:53:16 +0530

    initialized the app with baisc config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CustomerComponent } from './components/customer/customer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'customer',
+    loadComponent: () =>
+      import('./components/customer/customer.component').then(
+        (m) => m.CustomerComponent
+      ),
+  },
+];
